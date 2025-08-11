fde83dc51f3c883afe459912807520a5d9baf8ce
likai <1175474595@qq.com>
2025-08-11 20:23:53 +0800

    update magefile

--- magefile.go
+++ magefile.go
@@ -4,53 +4,25 @@
 package main
 
 import (
-	"github.com/openimsdk/gomake/mageutil"
 	"os"
 	"strings"
+
+	"github.com/openimsdk/gomake/mageutil"
 )
 
 var Default = Build
 
-var Aliases = map[string]any{
-	"buildcc": BuildWithCustomConfig,
-	"startcc": StartWithCustomConfig,
-}
-
-var (
-	customRootDir   = "."       // workDir in mage, default is "./"(project root directory)
-	customSrcDir    = "cmd"     // source code directory, default is "cmd"
-	customOutputDir = "_output" // output directory, default is "_output"
-	customConfigDir = "config"  // configuration directory, default is "config"
-	customToolsDir  = "tools"   // tools source code directory, default is "tools"
-)
-
-// Build support specifical binary build.
-//
-// Example: `mage build chat-api chat-rpc check-component`
 func Build() {
-	flag.Parse()
-	bin := flag.Args()
-	if len(bin) != 0 {
-		bin = bin[1:]
-	}
-	mageutil.Build(bin, nil)
-}
-
-func BuildWithCustomConfig() {
-	flag.Parse()
-	bin := flag.Args()
-	if len(bin) != 0 {
-		bin = bin[1:]
+	platforms := os.Getenv("PLATFORMS")
+	if platforms == "" {
+		platforms = mageutil.DetectPlatform()
 	}
 
-	config := &mageutil.PathOptions{
-		RootDir:   &customRootDir,
-		OutputDir: &customOutputDir,
-		SrcDir:    &customSrcDir,
-		ToolsDir:  &customToolsDir,
+	for _, platform := range strings.Split(platforms, " ") {
+		mageutil.CompileForPlatform(platform)
 	}
 
-	mageutil.Build(bin, config)
+	mageutil.PrintGreen("All binaries under cmd and tools were successfully compiled.")
 }
 
 func Start() {
@@ -61,37 +33,6 @@ func Start() {
 		os.Exit(1)
 	}
 	mageutil.StartToolsAndServices()
-
-	flag.Parse()
-	bin := flag.Args()
-	if len(bin) != 0 {
-		bin = bin[1:]
-	}
-
-	mageutil.StartToolsAndServices(bin, nil)
-}
-
-func StartWithCustomConfig() {
-	mageutil.InitForSSC()
-	err := setMaxOpenFiles()
-	if err != nil {
-		mageutil.PrintRed("setMaxOpenFiles failed " + err.Error())
-		os.Exit(1)
-	}
-
-	flag.Parse()
-	bin := flag.Args()
-	if len(bin) != 0 {
-		bin = bin[1:]
-	}
-
-	config := &mageutil.PathOptions{
-		RootDir:   &customRootDir,
-		OutputDir: &customOutputDir,
-		ConfigDir: &customConfigDir,
-	}
-
-	mageutil.StartToolsAndServices(bin, config)
 }
 
 func Stop() {
